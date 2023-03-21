24bfd7017a088a60afcf848fc70cb902d73fa2bb
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2023-03-21 22:11:07 +0200

    done

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,20 +10,130 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
-  reset: boolean = true;
+  forgot: boolean = true;
   verification: boolean = false;
   newPassword: boolean = false;
 
+  public forgotForm!: FormGroup;
+  public verificationForm!: FormGroup;
+  public newPasswordForm!: FormGroup;
+
+  url: string = 'http://localhost:5000/api/v1/auth/forgotPassword/';
+  url2: string = 'http://localhost:5000/api/v1/auth/codeVerification/';
+  url3: string = 'http://localhost:5000/api/v1/auth/resetPassword/';
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    public myService: AuthService
+  ) {}
   
-  verificationFunction() {
-    this.verification = true;
-    this.reset = false;
-    this.newPassword = false;
+  ngOnInit(): void {
+    this.forgotForm = this.formBuilder.group({
+      email: ''
+    });
+    this.verificationForm = this.formBuilder.group({
+      code: ''
+    });
+    this.newPasswordForm = this.formBuilder.group({
+      newPassword: '',
+      cnewPassword: '',
+    });
+  }
+  data: any;
+  errorMessageEmail: String = '';
+  errorMessageCode: String = '';
+  errorMessageCNewPassword: String = '';
+  errorMessageNewPassword: String = '';
+  successMessage: String = '';
+
+  forgotFunction(){
+    this.data=this.forgotForm.value;
+    console.log(this.data);
+    return this.http.post<any>(this.url,this.data).subscribe(
+      (data)=>{
+        console.log(data);
+        if (data.err){
+          console.log(data.err);
+          if(data.err[0][0].message=='"email" is not allowed to be empty' ||
+          data.err[0][0].message == '"email" must be a valid email'){
+            this.errorMessageEmail = 'Please enter a valid email';
+          }
+          
+        }
+        else{
+          this.forgot=false;
+          this.verification=true;
+          localStorage.setItem('email',this.data.email)
+        }
+      },(err)=>{
+        console.log(err);
+        if(err.status==404){
+          this.errorMessageEmail = 'Email Not Found';
+
+        }
+      }
+    )
+    
+  }
+  verificationFunction(){
+    this.data=this.verificationForm.value;
+    console.log(this.data);
+    return this.http.patch<any>(this.url2,this.data).subscribe(
+      (data)=>{
+        console.log(data);
+        if(data.err){
+          if(data.err[0][0].message=='"code" is required'||data.err[0][0].message=='"code" must be a number'){
+            this.errorMessageCode="Please enter the code."
+          }
+          
+        }else{
+          this.verification=false;
+          this.newPassword=true
+          
+        }
+      },(err)=>{
+        console.log(err);
+        if(err.status==404){
+          this.errorMessageCode="Wrong Code"
+
+        }
+      }
+    )
+    
   }
 
-  newPasswordFunction() {
-    this.newPassword = true;
-    this.verification = false;
-    this.reset = false;
+  newPasswordFunction(){
+    this.data=this.newPasswordForm.value;
+    console.log(this.data);
+    return this.http.patch<any>(this.url3+localStorage.getItem('email'),this.data).subscribe(
+      (data)=>{
+        console.log(data);
+       if(data.err){
+        if(data.err[0][0].message=='"newPassword" is not allowed to be empty'){
+          this.errorMessageCNewPassword = "";
+          this.errorMessageNewPassword='Please type password'
+        }else if(data.err[0][0].message=='"cnewPassword" must be [ref:newPassword]'){
+          this.errorMessageCNewPassword = "Password doesn't match";
+          this.errorMessageNewPassword='';
+        }
+       }
+       else{
+        this.newPasswordForm.reset();
+        this.successMessage="Password reset successfully, "
+       }
+      },(err)=>{
+        console.log(err);
+        
+      }
+    )
+    
   }
+
+
+
+
+ 
+
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   public loginForm!: FormGroup;
@@ -22,64 +22,58 @@ export class LoginComponent {
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: '',
-      password: ''
+      password: '',
     });
   }
 
-st: any;
+  st: any;
   errorMessageEmail: String = '';
   errorMessagePassword: String = '';
   errorMessageRegister: String = '';
 
-
-  login(){
-    this.st=this.loginForm.value;
+  login() {
+    this.st = this.loginForm.value;
     console.log(this.st);
-    return this.http.post<any>(this.url,this.st).subscribe(
-      (st)=>{
-        if(st.err){
+    return this.http.post<any>(this.url, this.st).subscribe(
+      (st) => {
+        if (st.err) {
           console.log(st.err);
-          if(st.err[0][0].message=='\"email\" is not allowed to be empty'||st.err[0][0].message == '"email" must be a valid email'){
+          if (
+            st.err[0][0].message == '"email" is not allowed to be empty' ||
+            st.err[0][0].message == '"email" must be a valid email'
+          ) {
             this.errorMessageEmail = 'Please enter a valid email';
-            this.errorMessagePassword= '';
+            this.errorMessagePassword = '';
+            this.errorMessageRegister = '';
+          } else if (
+            st.err[0][0].message == '"password" is not allowed to be empty'
+          ) {
+            this.errorMessageEmail = '';
+            this.errorMessagePassword = 'Please type your password,';
             this.errorMessageRegister = '';
           }
-        
-        else if (
-          st.err[0][0].message == '"password" is not allowed to be empty'
-          ) {
-          this.errorMessageEmail = '';
-          this.errorMessagePassword= 'Please type your password,';
-          this.errorMessageRegister = '';
-          
-        }}
-        else{
-          console.log("success",st.token);
+        } else {
+          console.log('success', st.token);
           this.loginForm.reset();
-          localStorage.setItem('userToken',st.token);
+          localStorage.setItem('userToken', st.token);
           this.myService.saveUserData();
           this.router.navigate(['/home']);
         }
       },
       (err) => {
-        console.log(err,err.error.message);
-        if(err.error.message=='Please register first.'){
+        console.log(err, err.error.message);
+        if (err.error.message == 'Please register first.') {
           this.errorMessageEmail = '';
-          this.errorMessagePassword= '';
+          this.errorMessagePassword = '';
           this.errorMessageRegister = ' ,Please Signup first';
-          
-        }
-        else if(err.error.message=='Ektb el password sa7.'){
+        } else if (err.error.message == 'Ektb el password sa7.') {
           this.errorMessageEmail = '';
-          this.errorMessagePassword= 'Wrong password';
+          this.errorMessagePassword = 'Wrong password';
           this.errorMessageRegister = '';
-
+        } else {
+          console.log('err', err);
         }
-        else {
-          console.log('err', err);}
       }
-    )
-    
+    );
   }
-
 }

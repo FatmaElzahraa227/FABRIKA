615c8f2af5c970c1ee56da63d093bdc3897ffc08
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2023-03-21 22:14:43 +0200

    done

--- modules/auth/auth.router.js
+++ modules/auth/auth.router.js
@@ -1,14 +1,14 @@
-const { signUp, signIn, sendCode,  resetPassword, /*confirmEmail, verifyCode, updateEmail*/ } = require('./controller/auth.controller');
+const { signUp, signIn, forgotPassword,  resetPassword, codeVerification, /*confirmEmail, verifyCode, updateEmail*/ } = require('./controller/auth.controller');
 const router = require('express').Router();
 const authValidation = require("./auth.validator");
 const validationFun = require('../../middleware/validation');
-
 router.post("/signUp",validationFun(authValidation.signUp),signUp);
 router.post("/signIn",validationFun(authValidation.signIn),signIn);
 // router.get("/confirm/:token", confirmEmail);
 // router.get("/confirm",confirmEmail);
-router.post("/sendCode",validationFun(authValidation.sendCode), sendCode);
-router.patch("/resetPassword/:useremail",validationFun(authValidation.forgetPassword),/*verifyCode,*/ resetPassword);
+router.post("/forgotPassword",validationFun(authValidation.forgotPassword), forgotPassword);
+router.patch("/resetPassword/:email",validationFun(authValidation.resetPassword),/*verifyCode,*/ resetPassword);
+router.patch("/codeVerification",validationFun(authValidation.codeVerification),/*verifyCode,*/ codeVerification);
 //router.patch("/updateEmail", updateEmail);
 
 require("dotenv").config();

88e7272e31a78588e630273b2ef7f332870d55ac
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2023-04-16 03:02:27 +0200

    s

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const connect = require("./DB/connection");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = require("./DB/connection");
 const app = express();
 const allRoutes = require("./modules/index.router");
 app.use(express.json());
 let cors = require("cors");
 app.use(cors());
-// connect();
-const uri = "mongodb+srv://fatmaahmed2272001:xv9qDWOVDyPlfx8d@fabrika.l5bkt0g.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-  connectTimeoutMS: 30000, // 30 seconds
-  socketTimeoutMS: 30000, 
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admins").command({ ping: 1 });
-    await client.db('my-database');
-  
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir); 
+client();
 const vehicleModel = require("./DB/model/vehicle");
 
 app.use(function (req, res, next) {
@@ -55,8 +28,8 @@ app.use(function (req, res, next) {
 //  app.use(bodyParser.json())
 
 //  // Set EJS as templating engine
-//  app.set("view engine", "ejs"); 
- 
+//  app.set("view engine", "ejs");
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use("/api/v1/auth", allRoutes.authRouter);
@@ -65,7 +38,6 @@ app.use("/api/v1/vehicle", allRoutes.vehicleRouter);
 app.use("/api/v1/admin", allRoutes.adminRouter);
 const port = process.env.PORT;
 
-const server=app.listen(port, () =>
+app.listen(port, "0.0.0.0", () =>
   console.log("server is running on port " + port)
 );
-server.keepAliveTimeout = 65000;

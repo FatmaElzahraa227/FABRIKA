df9e2ede4dbe5081d3541cfa0cf57feb334c9202
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2023-04-16 02:51:23 +0200

    s

--- DB/model/user.js
+++ DB/model/user.js
@@ -23,7 +23,9 @@ const userSchema = new mongoose.Schema({
 })
 userSchema.pre("save", async function (next) {
    this.password = await bcrypt.hashSync(this.password,parseInt(process.env.saltRounds))
- })
+ }, {
+   collection: 'users' // specify the collection name
+ });
 const userModel = mongoose.model('user', userSchema)
 
 module.exports = userModel

8d48dee060ff51e1a3133bdbc3c5a63fdd1dd351
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2023-04-16 02:14:27 +0200

    s

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 require("dotenv").config();
 const express = require("express");
 const connect = require("./DB/connection");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const allRoutes = require("./modules/index.router");
 app.use(express.json());
 let cors = require("cors");
 app.use(cors());
-connect();
+// connect();
+const uri = "mongodb+srv://fatmaahmed2272001:xv9qDWOVDyPlfx8d@fabrika.l5bkt0g.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir); 
 const vehicleModel = require("./DB/model/vehicle");
 
 app.use(function (req, res, next) {
@@ -28,8 +51,8 @@ app.use(function (req, res, next) {
 //  app.use(bodyParser.json())
 
 //  // Set EJS as templating engine
-//  app.set("view engine", "ejs");
-
+//  app.set("view engine", "ejs"); 
+ 
 app.get("/api", (req, res) => res.send("Hello World!"));
 
 app.use("/api/v1/auth", allRoutes.authRouter);

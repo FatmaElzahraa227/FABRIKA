b4195dab961371e4849ad17aaea47ae5d5ff5cb9
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2023-04-16 03:34:08 +0200

    s

--- DB/connection.js
+++ DB/connection.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
 const connect = async() => {
-  try {
-    await mongoose.connect(process.env.DB_CONNECTION);
-    console.log("DB connected ...");
+  await mongoose
+    .connect(process.env.DB_CONNECTION)
+    .then((res) => console.log("DB connected ..."))
+    .catch((err) => console.log("Error connecting to DB", err));
     return mongoose;
-  } catch (err) {
-    console.log("Error connecting to DB", err);
-    throw err;
-  } finally {
-    await mongoose.disconnect();
-  }
+    
 };
+// console.log(res);
+ 
 
 module.exports = connect;
